aaa74616670c1f3104ad5f40a42ef2d56d1d9fc8
TestSubjector <f2015845@hyderabad.bits-pilani.ac.in>
2016-11-19 01:55:50 +0530

    Added some actual code

--- lb1/main.py
+++ lb1/main.py
@@ -1,1 +1,13 @@
-print("Testing");
+import matplotlib;
+import numpy
+
+def MatrixCreate(Rows,Columns):
+    ''' Intialize an array with element values being zero '''
+    A = [[0 for x in range(Rows)] for y in range(Columns)]
+    return A
+  
+print(MatrixCreate(5,5));
+ 
+ #def MatrixRandomize(V):
+ #    ''' Get a random value between 0 & 1 back'''
+     
