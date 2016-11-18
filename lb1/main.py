cf036d30b73445e87c0a15d26d9e9565938923f4
TestSubjector <f2015845@hyderabad.bits-pilani.ac.in>
2016-11-19 02:48:09 +0530

    Matrix now stores values between 0 & 1

--- lb1/main.py
+++ lb1/main.py
@@ -1,13 +1,19 @@
-import matplotlib;
-import numpy
+import matplotlib
+import random
 
 def MatrixCreate(Rows,Columns):
-    ''' Intialize an array with element values being zero '''
+    '''Intialize an array with element values being zero'''
     A = [[0 for x in range(Rows)] for y in range(Columns)]
     return A
-  
-print(MatrixCreate(5,5));
- 
- #def MatrixRandomize(V):
- #    ''' Get a random value between 0 & 1 back'''
-     
+
+
+def MatrixRandomize(V):
+    ''' Get a random value between 0 & 1 back'''
+    for x in range(len(V)):
+        for y in range(len(V[x])):
+            V[x][y] = random.random()
+    return V
+
+parent = MatrixCreate(1, 50) 
+parent = MatrixRandomize(parent)
+print(parent)
