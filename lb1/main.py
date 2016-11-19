390b741b936d4924e83d048b3d669022e1e918cc
TestSubjector <f2015845@hyderabad.bits-pilani.ac.in>
2016-11-19 09:39:06 +0530

    Made Basic Fitness Code

--- lb1/main.py
+++ lb1/main.py
@@ -1,7 +1,11 @@
-import matplotlib
-import random
+'''The Hill Climber'''
 
-def MatrixCreate(Rows,Columns):
+import random
+import copy
+import matplotlib
+
+
+def MatrixCreate(Rows, Columns):
     '''Intialize an array with element values being zero'''
     A = [[0 for x in range(Rows)] for y in range(Columns)]
     return A
@@ -14,6 +18,36 @@ def MatrixRandomize(V):
             V[x][y] = random.random()
     return V
 
-parent = MatrixCreate(1, 50) 
+def Fitness(W):
+    '''Finding Mean of Elements'''
+    store = 0
+    for x in range(len(W)):
+        for y in range(len(W[x])):
+            store += W[x][y]
+    store = store / len(W)
+    return store
+
+def MatrixProb(P, Prob):
+    '''Assign different value based on probability'''
+    P_Copy = copy.deepcopy(P)
+    for x in range(len(P_Copy)):
+        for y in range(len(P_Copy[x])):
+            if Prob > random.random():
+                P_Copy[x][y] = random.random()
+    return P_Copy
+
+#Sample for testing
+
+parent = MatrixCreate(1, 50)
 parent = MatrixRandomize(parent)
-print(parent)
+parentFitness = Fitness(parent)
+
+for currentGeneration in range(5000):
+    print(currentGeneration, parentFitness)
+    child = MatrixProb(parent, 0.05)
+    childFitness = Fitness(child)
+    if childFitness > parentFitness:
+        parent = child
+        parentFitness = childFitness
+
+
